a2f3865dddff81793675556c1ed4d3d7422ef872
Zupanic <nicolas.garant@gmail.com>
2024-01-10 17:08:31 -0500

    feat:rectangle

---
+++ rectangle.cpp
@@ -0,0 +1,9 @@
+/********
+ * Fichier: rectangle.cpp
+ * Auteurs: Nicolas Garant
+ * Date: 04 janvier 2024 (creation)
+ * Description: Implementation de la classe rectangle
+ ********/
+
+#include "rectangle.h"
+
